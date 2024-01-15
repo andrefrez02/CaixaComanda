5d93045755914ff54cea3c3f60ca3ede618c82b2
andrefrez02 <andreluizfrezarinidiasdasilva@gmail.com>
2024-01-15 16:08:35 -0300

    Merge dados banco

---
+++ DadosBanco/Dump20240115c/caixacomanda_tb_produtos.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: caixacomanda
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_produtos`
+--
+
+DROP TABLE IF EXISTS `tb_produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_produtos` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(255) NOT NULL,
+  `Descricao` varchar(500) DEFAULT NULL,
+  `Preco` decimal(9,2) NOT NULL DEFAULT '0.00',
+  `Grupo` varchar(255) NOT NULL,
+  `Foto` varchar(500) DEFAULT NULL,
+  `Peso` varchar(45) DEFAULT NULL,
+  `Quantidade` int NOT NULL DEFAULT '1',
+  `Observacao` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_UNIQUE` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_produtos`
+--
+
+LOCK TABLES `tb_produtos` WRITE;
+/*!40000 ALTER TABLE `tb_produtos` DISABLE KEYS */;
+INSERT INTO `tb_produtos` VALUES (3,'Filé de frango a parmegiana','Filé de frango preparado ao molho parmegiana com queijo parmesão.<br />Acompanhamentos: Arroz, feijão, filé de frango ao molho parmegiana.<br /><br />300g de Arroz<br />150g de Feijão<br />1 Filé de frango com molho (aproximadamente 150g)<br /><br />Obs: Foto ilustrativa.<br /><br />Serve 1 pessoa (aprox. 600g)<br />',30.00,'Pratos Principais','file-de-frango-a-parmegiana.png','400g',1,NULL),(15,'Sopa de Cebola','.',13.49,'Sopas','sopa-de-cebola.jpeg','01.00',1,'teste'),(22,'Batata','Batata',30.50,'Acompanhamentos','batata frita.jpg','500',2,'Picante'),(23,'Peixe Frito','Peixe frito ',19.99,'Peixes','peixe-frito.webp','250g',1,'1 Peixe Frito Grande'),(24,'Saladinha','Salada saladinha bem temperadinha...',39.99,'Saladas','salada.webp','1.5Kg',5,'... com sal, pimenta, fogo, foguinho e FOGÃO!'),(25,'Suco de laranja','Suco com poupa de laranja.',4.00,'.','ft_6.png','1',1,'delicioso.');
+/*!40000 ALTER TABLE `tb_produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-15 16:07:48
